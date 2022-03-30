ca1e2278a1aba3bb2a8af8a06d6888c4b4879097
Ge Yang <yangge1987@gmail.com>
2022-03-29 21:27:48 -0400

    add parent directory

--- kaze/curl_utils.py
+++ kaze/curl_utils.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import requests
 from tqdm import tqdm
 
@@ -8,6 +10,10 @@ def download(url="http://www.ovh.net/files/10Mb.dat", filename='test.dat'):
     total_size_in_bytes = int(response.headers.get('content-length', 0))
     block_size = 1024  # 1 Kibibyte
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+
+    # make the parent directory
+    Path(filename).parent.mkdir()
+
     with open(filename, 'wb') as file:
         for data in response.iter_content(block_size):
             progress_bar.update(len(data))
